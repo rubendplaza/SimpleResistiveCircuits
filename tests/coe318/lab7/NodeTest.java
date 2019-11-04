0617f61e9887a9cec5f13708e78d001cb55e6ffe
Ruben Plaza <ruben.d.plaza@ryerson.ca>
2019-11-03 21:39:54 -0500

    added junit testing for the node class

---
+++ tests/coe318/lab7/NodeTest.java
@@ -0,0 +1,16 @@
+package coe318.lab7;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NodeTest {
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void getNodeNum() {
+    }
+}
